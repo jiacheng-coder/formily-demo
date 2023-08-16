dba30cdea59dc9d40f07aaf184fd471fbfb4584c
zhujiacheng06 <zhujiacheng06@meituan.com>
2023-08-16 11:27:03 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import Test from './pages/Test'
-import AsynchronousDataValidation from '@/pages/AsynchronousDataValidation'
+import Test from '@/pages/Test'
+import Login from '@/pages/Login'
 import LinkageEffectTest from '@/pages/LinkageEffectTest'
-import Login from './pages/Login'
+import AsynchronousDataValidation from '@/pages/AsynchronousDataValidation'
 
 export default function App() {
   return (
-    <>
+    <main>
       <header>
         <h1 style={{ textAlign: 'center' }}>Formily</h1>
       </header>
       <section style={{ padding: '0 50px' }}>
         {/* <Test /> */}
-        {/* <Login /> */}
-        <LinkageEffectTest />
+        <Login />
+        {/* <LinkageEffectTest /> */}
         {/* <AsynchronousDataValidation /> */}
       </section>
-    </>
+    </main>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
+// fileURLToPath: 是 Node.js 中的 url 模块提供的一个方法，用于将文件 URL 转换为文件系统路径。
+// new URL('./src', import.meta.url): 创建一个新的 URL 对象，表示 src 目录的绝对路径。import.meta.url 是当前模块的 URL，用作基础 URL。
 export default defineConfig({
   plugins: [react()],
   resolve: {
